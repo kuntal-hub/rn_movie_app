c690327d0be03e093e3e90a271daa13f62b8ab38
kuntal <km7999319@gmail.com>
2025-12-05 00:41:49 +0530

    Add constants for icons and images, define movie interfaces, and implement API fetching logic
    - Created `icons.ts` and `images.ts` to manage asset imports.
    - Defined interfaces for `Movie`, `TrendingMovie`, `MovieDetails`, and `TrendingCardProps` in `interfaces.d.ts`.
    - Implemented TMDB API configuration and fetching functions in `api.ts` for movies and movie details.
    - Developed a custom hook `useFetch` for handling data fetching and state management.
    - Added TypeScript declaration for PNG module in `images.d.ts`.

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+    return (
+        <View className='flex-1 bg-primary'>
+            <Text>index</Text>
+        </View>
+    )
+}
+
+export default index
