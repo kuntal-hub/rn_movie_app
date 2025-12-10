78f9f00f1e84bf38e5ec24c14c5bce04b0cb39f1
kuntal <km7999319@gmail.com>
2025-12-10 09:33:43 +0530

    feat: implement movie search functionality and enhance movie display components

---
+++ components/SearchBar.tsx
@@ -0,0 +1,35 @@
+import { View, TextInput, Image } from "react-native";
+
+import { icons } from "@/constants/icons";
+
+interface Props {
+    placeholder: string;
+    value?: string;
+    onChangeText?: (text: string) => void;
+    onPress?: () => void;
+    autoFocus?: boolean;
+}
+
+const SearchBar = ({ placeholder, value, onChangeText, onPress, autoFocus = false }: Props) => {
+    return (
+        <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+            <Image
+                source={icons.search}
+                className="w-5 h-5"
+                resizeMode="contain"
+                tintColor="#AB8BFF"
+            />
+            <TextInput
+                onPress={onPress}
+                placeholder={placeholder}
+                value={value}
+                onChangeText={onChangeText}
+                className="flex-1 ml-2 text-white"
+                placeholderTextColor="#A8B5DB"
+                autoFocus={autoFocus}
+            />
+        </View>
+    );
+};
+
+export default SearchBar;
