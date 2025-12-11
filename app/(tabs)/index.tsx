f120fdd308914956c9b1e95ec31acef14a0dc44f
kuntal <km7999319@gmail.com>
2025-12-11 08:31:01 +0530

    feat: add trending movies feature with card component and update movie details view

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,23 +10,23 @@ import { useRouter } from "expo-router";
 
 import useFetch from "@/services/usefetch";
 import { fetchMovies } from "@/services/api";
-// import { getTrendingMovies } from "@/services/appwrite";
+import { getTrendingMovies } from "@/services/appwrite";
 
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 
 import SearchBar from "@/components/SearchBar";
 import MovieCard from "@/components/MovieCard";
-// import TrendingCard from "@/components/TrendingCard";
+import TrendingCard from "@/components/TrendingCard";
 
 const Index = () => {
     const router = useRouter();
 
-    //   const {
-    //     data: trendingMovies,
-    //     loading: trendingLoading,
-    //     error: trendingError,
-    //   } = useFetch(getTrendingMovies);
+    const {
+        data: trendingMovies,
+        loading: trendingLoading,
+        error: trendingError,
+    } = useFetch(getTrendingMovies);
 
     const {
         data: movies,
@@ -49,14 +49,14 @@ const Index = () => {
             >
                 <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
-                {moviesLoading ? (
+                {moviesLoading || trendingLoading ? (
                     <ActivityIndicator
                         size="large"
                         color="#0000ff"
                         className="mt-10 self-center"
                     />
-                ) : moviesError ? (
-                    <Text>Error: {moviesError?.message}</Text>
+                ) : moviesError || trendingError ? (
+                    <Text>Error: {moviesError?.message || trendingError?.message}</Text>
                 ) : (
                     <View className="flex-1 mt-5">
                         <SearchBar
@@ -66,27 +66,27 @@ const Index = () => {
                             placeholder="Search for a movie"
                         />
 
-                        {/* {trendingMovies && (
-              <View className="mt-10">
-                <Text className="text-lg text-white font-bold mb-3">
-                  Trending Movies
-                </Text>
-                <FlatList
-                  horizontal
-                  showsHorizontalScrollIndicator={false}
-                  className="mb-4 mt-3"
-                  data={trendingMovies}
-                  contentContainerStyle={{
-                    gap: 26,
-                  }}
-                  renderItem={({ item, index }) => (
-                    <TrendingCard movie={item} index={index} />
-                  )}
-                  keyExtractor={(item) => item.movie_id.toString()}
-                  ItemSeparatorComponent={() => <View className="w-4" />}
-                />
-              </View>
-            )} */}
+                        {trendingMovies && (
+                            <View className="mt-10">
+                                <Text className="text-lg text-white font-bold mb-3">
+                                    Trending Movies
+                                </Text>
+                                <FlatList
+                                    horizontal
+                                    showsHorizontalScrollIndicator={false}
+                                    className="mb-4 mt-3"
+                                    data={trendingMovies}
+                                    contentContainerStyle={{
+                                        gap: 26,
+                                    }}
+                                    renderItem={({ item, index }) => (
+                                        <TrendingCard movie={item} index={index} />
+                                    )}
+                                    keyExtractor={(item) => item.movie_id.toString()}
+                                    ItemSeparatorComponent={() => <View className="w-4" />}
+                                />
+                            </View>
+                        )}
 
                         <>
                             <Text className="text-lg text-white font-bold mt-5 mb-3">
